da5f2b52d1512f87b6a744454a7518ebbbfa6a61
Paprikar <40892869+Paprikar@users.noreply.github.com>
2020-06-19 02:59:25 +0300

    Style changes and code fine tuning

--- full_demo/script.js
+++ full_demo/script.js
@@ -1,5 +1,5 @@
 const CANVAS_SIZE = 280;
-const CANVAS_SCALE = 0.5;
+const CANVAS_IMG_SCALE = 1;
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -17,7 +17,7 @@ const loadingModelPromise = sess.loadModel("./onnx_model.onnx");
 // Add 'Draw a number here!' to the canvas.
 ctx.lineWidth = 28;
 ctx.lineJoin = "round";
-ctx.font = "28px sans-serif";
+ctx.font = "24px Arial";
 ctx.textAlign = "center";
 ctx.textBaseline = "middle";
 ctx.fillStyle = "#212121";
@@ -27,88 +27,79 @@ ctx.fillText("Draw a number here!", CANVAS_SIZE / 2, CANVAS_SIZE / 2);
 ctx.strokeStyle = "#212121";
 
 function clearCanvas() {
-  ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-  for (let i = 0; i < 10; i++) {
-    const element = document.getElementById(`prediction-${i}`);
-    element.className = "prediction-col";
-    element.children[0].children[0].style.height = "0";
-  }
+    ctx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    for (let i = 0; i < 10; i++) {
+        const element = document.getElementById(`prediction-${i}`);
+        element.className = "prediction-col";
+        element.children[0].children[0].style.height = "0";
+    }
 }
 
 function drawLine(fromX, fromY, toX, toY) {
-  // Draws a line from (fromX, fromY) to (toX, toY).
-  ctx.beginPath();
-  ctx.moveTo(fromX, fromY);
-  ctx.lineTo(toX, toY);
-  ctx.closePath();
-  ctx.stroke();
-  updatePredictions();
+    ctx.beginPath();
+    ctx.moveTo(fromX, fromY);
+    ctx.lineTo(toX, toY);
+    ctx.closePath();
+    ctx.stroke();
+    updatePredictions().then();
 }
 
 async function updatePredictions() {
-  // Get the predictions for the canvas data.
-  const imgData = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-  const input = new onnx.Tensor(new Float32Array(imgData.data), "float32");
+    // Get the predictions for the canvas data.
+    const imgData = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    const input = new onnx.Tensor(new Float32Array(imgData.data), "float32");
 
-  await loadingModelPromise;
-  const outputMap = await sess.run([input]);
-  const outputTensor = outputMap.values().next().value;
-  const predictions = outputTensor.data;
-  const maxPrediction = Math.max(...predictions);
+    await loadingModelPromise;
+    const outputMap = await sess.run([input]);
+    const outputTensor = outputMap.values().next().value;
+    const predictions = outputTensor.data;
+    const maxPrediction = Math.max(...predictions);
 
-  for (let i = 0; i < predictions.length; i++) {
-    const element = document.getElementById(`prediction-${i}`);
-    element.children[0].children[0].style.height = `${predictions[i] * 100}%`;
-    element.className =
-      predictions[i] === maxPrediction
-        ? "prediction-col top-prediction"
-        : "prediction-col";
-  }
+    for (let i = 0; i < predictions.length; i++) {
+        const element = document.getElementById(`prediction-${i}`);
+        element.children[0].children[0].style.height = `${predictions[i] * 100}%`;
+        element.className =
+            predictions[i] === maxPrediction
+                ? "prediction-col top-prediction"
+                : "prediction-col";
+    }
 }
 
 function canvasMouseDown(event) {
-  isMouseDown = true;
-  if (hasIntroText) {
-    clearCanvas();
-    hasIntroText = false;
-  }
-  const x = event.offsetX / CANVAS_SCALE;
-  const y = event.offsetY / CANVAS_SCALE;
+    isMouseDown = true;
+    if (hasIntroText) {
+        clearCanvas();
+        hasIntroText = false;
+    }
+    const x = event.offsetX / CANVAS_IMG_SCALE;
+    const y = event.offsetY / CANVAS_IMG_SCALE;
 
-  // To draw a dot on the mouse down event, we set laxtX and lastY to be
-  // slightly offset from x and y, and then we call `canvasMouseMove(event)`,
-  // which draws a line from (laxtX, lastY) to (x, y) that shows up as a
-  // dot because the difference between those points is so small. However,
-  // if the points were the same, nothing would be drawn, which is why the
-  // 0.001 offset is added.
-  lastX = x + 0.001;
-  lastY = y + 0.001;
-  canvasMouseMove(event);
+    drawLine(x, y, x + 1, y + 1)
 }
 
 function canvasMouseMove(event) {
-  const x = event.offsetX / CANVAS_SCALE;
-  const y = event.offsetY / CANVAS_SCALE;
-  if (isMouseDown) {
-    drawLine(lastX, lastY, x, y);
-  }
-  lastX = x;
-  lastY = y;
+    const x = event.offsetX / CANVAS_IMG_SCALE;
+    const y = event.offsetY / CANVAS_IMG_SCALE;
+    if (isMouseDown) {
+        drawLine(lastX, lastY, x, y);
+    }
+    lastX = x;
+    lastY = y;
 }
 
 function bodyMouseUp() {
-  isMouseDown = false;
+    isMouseDown = false;
 }
 
 function bodyMouseOut(event) {
-  // We won't be able to detect a MouseUp event if the mouse has moved
-  // ouside the window, so when the mouse leaves the window, we set
-  // `isMouseDown` to false automatically. This prevents lines from
-  // continuing to be drawn when the mouse returns to the canvas after
-  // having been released outside the window.
-  if (!event.relatedTarget || event.relatedTarget.nodeName === "HTML") {
-    isMouseDown = false;
-  }
+    // We won't be able to detect a MouseUp event if the mouse has moved
+    // outside the window, so when the mouse leaves the window, we set
+    // `isMouseDown` to false automatically. This prevents lines from
+    // continuing to be drawn when the mouse returns to the canvas after
+    // having been released outside the window.
+    if (!event.relatedTarget || event.relatedTarget.nodeName === "HTML") {
+        isMouseDown = false;
+    }
 }
 
 canvas.addEventListener("mousedown", canvasMouseDown);
